ce80e26c10e9398aa5b174ee819cea2515b7dc0e
Abdul Rauf <sigidevelopers@gmail.com>
2024-11-25 10:49:55 +0500

    check

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4003;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
